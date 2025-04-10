465559bdd5ab609e65fbedbe4c370ddc49bcfa26
Jotiram75 <shindejotiram13032003@gmail.com>
2025-04-10 22:44:21 +0530

    new js-2

--- 10JS/3app.js
+++ 10JS/3app.js
@@ -189,3 +189,46 @@ console.log(gamettt);
 
 console.log(gamettt[0][1]='0');
 
+
+//loops
+for(let i=0;i<=10;i++){
+    console.log(i);
+}
+
+for(let i=15;i>=1;i--){
+    console.log(i);
+}
+
+
+//odd numbers
+for(let i=1;i<=15;i=i+2) {
+    console.log(i);
+}
+
+console.log("Backward");
+for(let i=15;i>=1;i=i-2) {
+    console.log(i);
+}
+console.log("even");
+//even numbers
+for(let i=2;i<=10;i=i+2) {
+    console.log(i);
+}
+console.log("Backward");
+for(let i=10;i>=2;i=i-2) {
+    console.log(i);
+}
+console.log("multiplication");
+for(let i=5;i<=50;i=i+5) {
+    console.log(i);
+}
+
+let n=prompt("Write your number");
+n=parseInt(n);
+console.log("Table of "+n);
+
+for(let i=n;i<=n*10;i=i+n){
+    console.log(i);
+}
+
+//nested loops
