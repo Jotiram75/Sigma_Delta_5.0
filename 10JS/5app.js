cd95cf09cd3de94e0ed65a8f1cbdd4f53fbefcb8
Jotiram75 <shindejotiram13032003@gmail.com>
2025-04-12 21:44:11 +0530

    new js-5

--- 10JS/5app.js
+++ 10JS/5app.js
@@ -40,3 +40,86 @@ console.log(student);
 
 delete student.marks;
 console.log(student);
+
+
+//object of object
+
+const info={
+    joti:{
+        name:'joti',
+        blood:'O',
+        city:'nevare'
+    },
+    ram:{
+        name:'ram',
+        blood:'A',
+        city:'nevare'
+    },
+    sham:{
+        name:'sham',
+        blood:'B',
+        city:'nevare'
+    }
+}
+console.log(info);
+
+
+//array of object
+
+let Array=[
+    {
+        name:'joti',
+        blood:'O',
+        city:'nevare'
+    },
+    {
+        name:'ram',
+        blood:'A',
+        city:'nevare'
+    },
+    {
+        name:'sham',
+        blood:'B',
+        city:'nevare'
+    }
+]
+console.log(Array);
+
+//Maths Object
+console.log(Math);
+let random=Math.random();
+//1-10
+console.log(Math.floor(random*10)+1);
+//1-100
+console.log(Math.floor(random*100)+1);
+//1-5
+console.log(Math.floor(random*5)+1);
+//20-25
+console.log(Math.floor(random*5)+21);
+
+
+//Guessing Game
+
+const max=prompt("Enter Max number");
+const random1=Math.floor(Math.random()*max)+1;
+let guess=prompt("Guess the number");
+
+while(true){
+    if(guess == "quit"){
+        console.log("User Quit");
+        break;
+    }
+
+    if(guess==random1){
+        console.log("You Are Right! Congrats!!");
+        break;
+    }else if(guess<random1){
+        guess=prompt("You Guess Was Too Small");
+    }else if(guess>random1){
+        guess=prompt("You Guess Was Too Large");
+    }
+    else{
+        guess=prompt("Your Guess Was Wrong.Please Try Again");
+    }
+}
+
